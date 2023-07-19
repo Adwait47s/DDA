0e6cc1c1ed89fe25d834cad231896f84facc66dd
Adwait Nagarkar <109037318+Adwait47s@users.noreply.github.com>
2023-07-19 15:37:43 +0530

    Update greedy.cpp

--- Assignment 1/greedy.cpp
+++ Assignment 1/greedy.cpp
@@ -5,16 +5,55 @@ int main(){
     int n;
     cin>>n;
     cout << "Please enter the profit of each item :" << endl;
-    vector<pair<int,int>>v;
+    vector<double>profit,weight;
+    map<pair<int,int>,int>items;
     for(int i=0;i<n;i++){
         int a; cin>>a;
-        v.push_back({a,0});
+        profit.push_back(a);
     } 
     cout << "Please the cost of each item :" << endl;
     for(int i=0;i<n;i++){
-        int a;
+        double a;
         cin>>a;
-        v[i].second=a;
+        weight.push_back(a);
+        items[{profit[i],a}]=i+1;
     }
-    vector<pair<int,vector<pair<int,int>>>
+    map<double,set<pair<int,int>>>m;
+    for(int i=0;i<n;i++){
+        double cost = profit[i]/ weight[i];
+        m[-cost].insert({profit[i],weight[i]});
+    }
+    cout << "Please enter the size of knapsack :" << endl;
+    double mm;
+    cin>>mm;
+    vector<pair<int,int>>ans;
+    vector<double>quantity;
+    for(auto it:m){
+    	for(auto itt:it.second){
+    		double weightt = itt.second;
+    		if(weightt<=mm){
+    			mm-=weightt;
+    			ans.push_back({itt.first,itt.second});
+    			quantity.push_back(1);
+    		}
+    		else{
+    			double taken = mm/itt.second;
+    			ans.push_back({itt.first,itt.second});
+    			quantity.push_back(taken);
+    			mm=0;
+    			break;
+    		}
+    	}
+    }
+    double profitt=0;
+    for(int i=0;i<ans.size();i++){
+    	profitt+= (ans[i].first*quantity[i]);
+    }
+    cout << "Items taken and Quantity" << endl;
+    for(int i=0;i<ans.size();i++){
+    	cout << items[ans[i]] << "  " << quantity[i] << endl;
+    }
+    cout << endl;
+    cout << "Profit is :";
+    cout << profitt << endl;
 }
